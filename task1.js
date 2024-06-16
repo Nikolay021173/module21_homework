f21cc6cc09e6525bf306ebf09b2293276d70c74d
Nikolay021173 <nikolay021173@gmail.com>
2024-06-16 20:19:17 +0300

    A small fix in task 1 of module 21 AJAX Asynchrony

--- task1.js
+++ task1.js
@@ -1,23 +1,4 @@
-/*const xmlStr = `<list>
-  <student>
-    <name lang="en">
-      <first>Ivan</first>
-      <second>Ivanov</second>
-    </name>
-    <age>35</age>
-    <prof>teacher</prof>
-  </student>
-  <student>
-    <name lang="ru">
-      <first>Петр</first>
-      <second>Петров</second>
-        </name>
-        <age>58</age>
-        <prof>driver</prof>
-        </student>
-    </list>`;*/
-
-    const xmlStr = `<list>
+   const xmlStr = `<list>
               <student>
                   <name lang="en">
                       <first>Ivan</first>
